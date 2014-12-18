2a660c525fef3e793019f87d0ff4e08e018d4aad
iofjuupasli <iofjuupasli@gmail.com>
2014-12-18 18:47:15 +0300

    components

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 (function () {
     var h = React.createElement;
+
     var Timeline = React.createClass({
         render: function () {
             return h('div', null, 'Timeline');
         }
     });
+    var Map = React.createClass({
+        render: function () {
+            return h('div', null, 'Map');
+        }
+    });
+    var Persons = React.createClass({
+        render: function () {
+            return h('div', null, 'Persons');
+        }
+    });
+    var Cloud = React.createClass({
+        render: function () {
+            return h('div', null, 'Cloud');
+        }
+    });
+    var Wiki = React.createClass({
+        render: function () {
+            return h('div', null, 'Wiki');
+        }
+    });
+
     var App = React.createClass({
         render: function () {
             return h('div', {
@@ -17,24 +39,36 @@
                         className: 'column column-second'
                     },
                     h('div', {
-                        className: 'row row-map'
-                    }, 'Map'),
+                            className: 'row row-map'
+                        },
+                        h(Map)
+                    ),
                     h('div', {
-                        className: 'row row-persons'
-                    }, 'Persons')
+                            className: 'row row-persons'
+                        },
+                        h(Persons)
+                    )
                 ),
                 h('div', {
                         className: 'column column-third'
                     },
                     h('div', {
-                        className: 'row row-cloud'
-                    }, 'Cloud'),
+                            className: 'row row-cloud'
+                        },
+                        h(Cloud)
+                    ),
                     h('div', {
-                        className: 'row row-wiki'
-                    }, 'Wiki')
+                            className: 'row row-wiki'
+                        },
+                        h(Wiki)
+                    )
                 )
             );
         }
     });
-    React.render(h(App), document.getElementById('application'));
+
+    React.render(
+        h(App),
+        document.getElementById('application')
+    );
 }());
