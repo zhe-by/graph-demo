d2a84d23795e4d7ead7d04bf8c40924d0d86c459
iofjuupasli <iofjuupasli@gmail.com>
2014-12-22 23:20:26 +0300

    select range

--- app/Timeline.js
+++ app/Timeline.js
@@ -1,9 +1,43 @@
 (function () {
     'use strict';
+    var PxDateMixin = {
+        getPxForDate: function (date) {
+            return zhe.Timeline.getPxForDate(
+                this.getDOMNode().offsetHeight,
+                this.props.start,
+                this.props.end,
+                date);
+        },
+        getDateForPx: function (px) {
+            return zhe.Timeline.getDateForPx(
+                this.getDOMNode().offsetHeight,
+                this.props.start,
+                this.props.end,
+                px);
+        }
+    };
+
     var TimelineScale = React.createClass({
-        mixins: [React.addons.PureRenderMixin],
+        mixins: [
+            React.addons.PureRenderMixin,
+            PxDateMixin
+        ],
+        getInitialState: function () {
+            return {
+                newStart: null,
+                newEnd: null
+            };
+        },
         render: function () {
+            if (!this.isMounted()) {
+                setTimeout(this.forceUpdate.bind(this), 0);
+                return h('div', {
+                    className: 'timeline-scale'
+                });
+            }
+
             var events = this.props.events;
+
             var lines = _(events)
                 .sortBy('importance')
                 .map(function (event) {
@@ -12,10 +46,9 @@
                             className: 'event event-once-line',
                             style: {
                                 height: zhe.Timeline.getLineHeightByImportance(event.importance),
-                                top: this.props.getPxForDate(event.date),
+                                top: this.getPxForDate(event.date),
                                 backgroundColor: zhe.Timeline.getEventColorByImportance(event.importance)
                             }
-                            // onMouseOver: this.props.onHoverLine.bind(null, events.indexOf(event))
                         });
                     } else if (event.type === 'eventLong') {
                         // todo
@@ -24,48 +57,113 @@
                     }
                 }.bind(this))
                 .valueOf();
+            var zoom;
+            if (this.state.newStart) {
+                zoom = h('div', {
+                    className: 'timeline-scale-zoom',
+                    style: {
+                        top: this.getPxForDate(this.state.newStart),
+                        height: this.getPxForDate(this.state.newEnd) -
+                            this.getPxForDate(this.state.newStart)
+                    }
+                });
+            }
             return h('div', {
-                className: 'timeline-scale',
-                onMouseMove: this.onHover,
-                onWheel: function (e) {
-                    var hoveredIndex = this._findIndexClosest(e.clientY);
-                    this.props.onZoom(e.deltaY > 0 ? 1 : -1, this.props.events[hoveredIndex].date);
-                }.bind(this),
-                onClick: function (e) {
-                    var hoveredIndex = this._findIndexClosest(e.clientY);
-                    this.props.onSelect(this.props.events[hoveredIndex]);
-                }.bind(this)
-            }, lines);
+                    className: 'timeline-scale',
+                    onMouseMove: this.onHover,
+                    onWheel: this.onWheel,
+                    onClick: this.onClick,
+                    onMouseDown: function (e) {
+                        var startPos = e.clientY;
+                        var MIN_ZOOM = 50;
+
+                        var move = function (e) {
+                            var newPos = e.clientY;
+                            if (Math.abs(startPos - newPos) > MIN_ZOOM) {
+                                this.setState({
+                                    newStart: this.getDateForPx(startPos > newPos ? newPos : startPos),
+                                    newEnd: this.getDateForPx(startPos > newPos ? startPos : newPos)
+                                });
+                            }
+                        }.bind(this);
+
+                        var end = function (e) {
+                            document.body.removeEventListener('mouseup', end);
+                            document.body.removeEventListener('mousemove', move);
+                            var endPos = e.clientY;
+                            if (Math.abs(startPos - endPos) > MIN_ZOOM) {
+                                this.props.onZoom(this.state.newStart, this.state.newEnd);
+                            }
+                            this.setState({
+                                newStart: null,
+                                newEnd: null
+                            });
+                        }.bind(this);
+
+                        document.body.addEventListener('mouseup', end);
+                        document.body.addEventListener('mousemove', move);
+                    }.bind(this)
+                },
+                h('div', null, lines),
+
+                !!this.state.newStart && zoom
+            );
         },
-        _findIndexClosest: function (y) {
-            return _.reduce(this.props.events, function (result, event, i) {
-                var lineY = this.props.getPxForDate(event.date);
-                if (Math.abs(lineY - y) < result.min) {
-                    result.min = Math.abs(lineY - y);
-                    result.index = i;
-                }
-                return result;
-            }.bind(this), {
-                min: Infinity,
-                index: -1
-            }).index;
+        findClosest: function (y) {
+            return _.reduce(this.props.events,
+                function (result, event, i) {
+                    var lineY = this.getPxForDate(event.date);
+                    if (Math.abs(lineY - y) < result.min) {
+                        result.min = Math.abs(lineY - y);
+                        result.index = i;
+                        result.value = event;
+                    }
+                    return result;
+                }.bind(this), {
+                    min: Infinity,
+                    index: -1,
+                    value: undefined
+                });
         },
         onHover: function (e) {
-            var hoveredIndex = this._findIndexClosest(e.clientY);
-            this.props.onHoverLine(hoveredIndex);
+            var hovered = this.findClosest(e.clientY).value;
+            this.props.onHoverLine(hovered);
+        },
+        onWheel: function (e) {
+            var hovered = this.findClosest(e.clientY).value;
+            var zoom = e.deltaY > 0 ? 1 : -1;
+
+            var start = hovered.date - (hovered.date - this.props.start) * (zoom > 0 ? 1.1 : 0.9);
+            var end = hovered.date + (this.props.end - hovered.date) * (zoom > 0 ? 1.1 : 0.9);
+
+            this.props.onZoom(start, end);
+        },
+        onClick: function (e) {
+            var hovered = this.findClosest(e.clientY).value;
+            this.props.onSelect(hovered);
         }
     });
 
     var TimelineTitles = React.createClass({
-        mixins: [React.addons.PureRenderMixin],
+        mixins: [
+            React.addons.PureRenderMixin,
+            PxDateMixin
+        ],
         render: function () {
+            if (!this.isMounted()) {
+                setTimeout(this.forceUpdate.bind(this), 0);
+                return h('div', {
+                    className: 'timeline-titles-statics'
+                });
+            }
+
             var renderedTitles = [];
             var titles = _(this.props.events)
                 .sortBy(function (event) {
                     return -event.importance;
                 })
                 .filter(function (event) {
-                    var newTop = this.props.getPxForDate(event.date);
+                    var newTop = this.getPxForDate(event.date);
                     if (newTop - this.props.titleSize <= 0) {
                         return false;
                     }
@@ -82,7 +180,7 @@
                     return h('div', {
                         className: 'event-title',
                         style: {
-                            top: this.props.getPxForDate(event.date) - this.props.titleSize,
+                            top: this.getPxForDate(event.date) - this.props.titleSize,
                             borderBottomWidth: zhe.Timeline.getLineHeightByImportance(event.importance),
                             borderColor: zhe.Timeline.getEventColorByImportance(event.importance)
                         }
@@ -96,12 +194,22 @@
     });
 
     var TimelineOver = React.createClass({
-        mixins: [React.addons.PureRenderMixin],
+        mixins: [
+            React.addons.PureRenderMixin,
+            PxDateMixin
+        ],
         render: function () {
+            if (!this.isMounted()) {
+                setTimeout(this.forceUpdate.bind(this), 0);
+                return h('div', {
+                    className: 'event-over-container'
+                });
+            }
+
             var titleOver = h('div', {
                     className: 'event-title event-over',
                     style: {
-                        top: this.props.getPxForDate(this.props.event.date) - this.props.titleSize,
+                        top: this.getPxForDate(this.props.event.date) - this.props.titleSize,
                         borderBottomWidth: zhe.Timeline.getLineHeightByImportance(this.props.event.importance),
                         borderColor: zhe.Timeline.getEventColorByImportance(this.props.event.importance)
                     }
@@ -111,7 +219,7 @@
             );
 
             return h('div', {
-                    className: 'event-over'
+                    className: 'event-over-container'
                 },
                 titleOver
             );
@@ -137,6 +245,18 @@
                     return 2;
                 }
                 return 1;
+            },
+            getPxForDate: function (sizePx, start, end, date) {
+                function get(date) {
+                    return (date - start) / (end - start) * sizePx;
+                }
+                return date ? get(date) : get;
+            },
+            getDateForPx: function (sizePx, start, end, px) {
+                function get(px) {
+                    return (px / sizePx) * (end - start) + start;
+                }
+                return px ? get(px) : get;
             }
         },
         getDefaultProps: function () {
@@ -145,52 +265,24 @@
             };
         },
         getInitialState: function () {
+            var start = moment([1000]).unix();
+            var end = moment([2000]).unix();
             return {
-                start: moment([1000]).unix(),
-                end: moment([2000]).unix(),
-                events: zhe.graph.getEventsInRange(
-                    moment([1000]).unix(), moment([2000]).unix()),
-                getPxForDate: function (date) {
-                    var pxSize = this.getDOMNode().offsetHeight;
-                    var timeSize = this.state.end - this.state.start;
-                    var msPerPx = timeSize / pxSize;
-                    return (date - this.state.start) / msPerPx;
-                }.bind(this)
+                start: start,
+                end: end,
+                events: zhe.graph.getEventsInRange(start, end)
             };
         },
         render: function () {
-            if (!this.isMounted()) {
-                setTimeout(function () {
-                    this.forceUpdate();
-                }.bind(this), 0);
-                return h('div', {
-                    className: 'timeline'
-                });
-            }
-
             return h('div', {
                     className: 'timeline'
                 },
                 h(TimelineScale, {
                     events: this.state.events,
-                    getPxForDate: this.state.getPxForDate,
+                    start: this.state.start,
+                    end: this.state.end,
                     onHoverLine: this.onHoverLine,
-                    onZoom: function (zoom, zoomPosition) {
-                        var start = zoomPosition - (zoomPosition - this.state.start) * (zoom > 0 ? 1.1 : 0.9);
-                        var end = zoomPosition + (this.state.end - zoomPosition) * (zoom > 0 ? 1.1 : 0.9);
-
-                        this.setState({
-                            start: start,
-                            end: end,
-                            events: zhe.graph.getEventsInRange(start, end),
-                            getPxForDate: function (date) {
-                                var pxSize = this.getDOMNode().offsetHeight;
-                                var timeSize = this.state.end - this.state.start;
-                                var msPerPx = timeSize / pxSize;
-                                return (date - this.state.start) / msPerPx;
-                            }.bind(this)
-                        });
-                    }.bind(this),
+                    onZoom: this.onZoom,
                     onSelect: this.props.onSelect
                 }),
                 h('div', {
@@ -198,21 +290,30 @@
                     },
                     h(TimelineTitles, {
                         events: this.state.events,
-                        getPxForDate: this.state.getPxForDate,
-                        titleSize: this.props.titleSize
+                        titleSize: this.props.titleSize,
+                        start: this.state.start,
+                        end: this.state.end
                     }),
 
                     !!this.state.over && h(TimelineOver, {
-                        event: this.state.events[this.state.over],
-                        getPxForDate: this.state.getPxForDate,
+                        event: this.state.over,
+                        start: this.state.start,
+                        end: this.state.end,
                         titleSize: this.props.titleSize
                     })
                 )
             );
         },
-        onHoverLine: function (i, e) {
+        onHoverLine: function (eventHovered) {
             this.setState({
-                over: i
+                over: eventHovered
+            });
+        },
+        onZoom: function (start, end) {
+            this.setState({
+                start: start,
+                end: end,
+                events: zhe.graph.getEventsInRange(start, end)
             });
         }
     });
