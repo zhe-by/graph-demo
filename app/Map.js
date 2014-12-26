296bc82db1e7ca97a4f715a3cc5823783c998e5b
YaGen <yahenmail@gmail.com>
2014-12-26 22:35:52 +0300

    map init

--- app/Map.js
+++ app/Map.js
@@ -1,8 +1,26 @@
 define(function (require) {
     'use strict';
+
+    var mapId = 'map';
+
+    function renderMap (elementId, objects) {
+        L.mapbox.accessToken = 'pk.eyJ1IjoieWFoZW4iLCJhIjoiRHlMalFDcyJ9.2NT-F0P1hgNDr7804742nQ';
+        L.mapbox.map(elementId, 'yahen.kigp3i1m');
+    }
+
     return React.createClass({
         render: function () {
-            return h('div', null, 'Map');
+            return h('div', {
+                id:mapId, 
+                className: 'map'
+            });
+        },
+        componentDidMount: function() {
+            renderMap(mapId, this.props.objects);
+        },
+        shouldComponentUpdate: function(props) {
+            renderMap(mapId, this.props.objects);   
+            return false;
         }
-    });
+    });    
 });
